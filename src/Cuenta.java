d0c5eea074edd008cc89476276e2c25e1c76a349
Veronica <vgalbicka@gmail.com>
2024-04-07 19:51:59 -0300

    update

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -8,16 +8,18 @@ public class Cuenta {
         String tipoDeCuenta = "Corriente";
         double saldo = 1599.99;
         int opcion =0;
+        double montoARetirar = 0;
+        double montoADepositar = 0;
 
-        System.out.println("\n*-*-*-*-*-*-*-*-*-*-*-*-*");
+        System.out.println("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
         System.out.println("\nNombre del Cliente: " + nombre);
         System.out.println("Tipo de cuenta: " + tipoDeCuenta);
         System.out.println("Saldo disponible: $ "+ saldo );
-        System.out.println("\n*-*-*-*-*-*-*-*-*-*-*-*-*");
+        System.out.println("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
 
         /* Text block*/
         String menu = """
-                *-*-* Escoja una opción*-*-*
+                \n*-*-* Escoja una opción *-*-*\n
                 1 - Consultar saldo
                 2 - Retiro
                 3 - Depositar
@@ -29,19 +31,36 @@ public class Cuenta {
 
             switch (opcion){
                 case 1:
-                    System.out.println("El saldo actual es: $ " + saldo);
+                    System.out.println("Su saldo es: $ " + saldo);
                     break;
+
                 case 2:
                     System.out.println("Ingrese el monto a retirar:");
-                    double montoARetirar = teclado.nextDouble();
-                    if (saldo){
-
+                    montoARetirar = teclado.nextDouble();
+                    if (saldo < montoARetirar){
+                        System.out.println("Saldo insuficiente");
+                    }else{
+                        saldo -=montoARetirar;
+                        System.out.println("Retire su dinero.");
+                        System.out.println("Su saldo es: $ " + saldo);
                     }
+                    break;
 
+                case 3:
+                    System.out.println("Ingrese el monto a depositar: $ ");
+                    montoADepositar = teclado.nextDouble();
+                    saldo +=montoADepositar;
+                    System.out.println("Su saldo es: $ " + saldo);
+                    break;
+
+                case 9:
+                    System.out.println("Cerrando sesión. No olvide retirar su tarjeta.");
+                    break;
+
+                default:
+                    System.out.println("La opción ingresada no está en el menú. Escoja una opción del menú.");
+                    break;
             }
-
-
         }
-
     }
 }
